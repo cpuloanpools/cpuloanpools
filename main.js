94f2b6aeecd0188c3267e1a319c6b0eb0d36d046
cpuloanpools <93321684+cpuloanpools@users.noreply.github.com>
2022-01-14 04:03:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,21 +10,6 @@ const pools = [
     url: "https://cpuloanpools.github.io/cpuloanpools/",
     contract: "cpuloanpools",
   },
-  {
-    name: "Pool 2",
-    url: "https://cpuloanpools.github.io/cpuloanpools/SecondPool/",
-    contract: "cpuloanpool2",
-  },
-  {
-    name: "Pool 3",
-    url: "https://cpuloanpools.github.io/cpuloanpools/ThirdPool/",
-    contract: "cpuloanpool3",
-  },
-  {
-    name: "Pool 4",
-    url: "https://cpuloanpools.github.io/cpuloanpools/FourthPool/",
-    contract: "cpuloanpool4",
-  },
 
   //{ name: "x2 pool", url: "/x2pool/", contract: "x2waxcpuloan" },
 ];
