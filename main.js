45ad99a8a479d2222209d788fe7858518241b438
cpuloanpools <93321684+cpuloanpools@users.noreply.github.com>
2022-03-28 19:08:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const transport = new AnchorLinkBrowserTransport();
 const anchorLink = new AnchorLink({
   transport,
   chains: [{
-    chainId: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
-    nodeUrl: 'https://wax.greymass.com',
+    chainId: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
+    nodeUrl: 'https://waxtestnet.greymass.com',
   }],
 });
 
 const dapp = "WaxCPULoan";
-const endpoint = "wax.eosphere.io";
+const endpoint = "waxtestnet.greymass.com";
 const tokenContract = { WAX: "eosio.token" };
 const menuPrices = [1, 2, 4];
 const default_wax_value = 100;
