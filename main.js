5214594990a2e521fef66fdbb7f5059e6ddb6387
cpuloanpools <93321684+cpuloanpools@users.noreply.github.com>
2022-04-11 16:19:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const endpoint = "https://wax.greymass.com"; // Main-net
-//const endpoint = "https://waxtestnet.greymass.com"; // Test-net
-const chainid = '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a'; //Main-net
-//const chainid = 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12'; //Test net
+//const endpoint = "https://wax.greymass.com"; // Main-net
+const endpoint = "https://waxtestnet.greymass.com"; // Test-net
+//const chainid = '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a'; //Main-net
+const chainid = 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12'; //Test net
 const wax = new waxjs.WaxJS({
   rpcEndpoint: "https://wax.greymass.com",
   tryAutoLogin: false
